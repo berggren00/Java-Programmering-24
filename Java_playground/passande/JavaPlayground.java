5516b976b8db7cb31651a53d5396f6ae6e6259d1
andreas <andreas_berggren@hotmail.com>
2024-09-01 19:04:40 +0200

    Uppdaterad
    La till mer rader

--- Java_playground/passande/JavaPlayground.java
+++ Java_playground/passande/JavaPlayground.java
@@ -1,5 +1,8 @@
 public class JavaPlayground {
     public static void main(String[] args) {
-      System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("What");
+        System.out.println("Man UTD");
+        System.out.println("Hej Världen");
     }
   }
