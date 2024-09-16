59b956dc96890f06b5bbd2f765526a05db22156d
andreas <andreas_berggren@hotmail.com>
2024-09-16 11:52:00 +0200

    book

---
+++ exercises/standard_exercises/Book.java
@@ -0,0 +1,12 @@
+package exercises.standard_exercises;
+
+public class Book { 
+    private String title;
+    private String author;
+    private int year;
+    public Book(){
+        title = "Default title";
+        author = "Default author";
+        year = 30;
+    }
+}
